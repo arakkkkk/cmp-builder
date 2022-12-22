598f8a0e9b96ab226f8d722d5da1ad922c7d1d40
kinoshitalab <kouiti@arakkk.local>
2022-12-22 10:15:30 +0900

    update for test

--- lua/cmp-builder.lua
+++ lua/cmp-builder.lua
@@ -2,7 +2,9 @@ M = {}
 function M.add(pattern_table, cmp_name, trigger_characters, test)
 	local cmp_table = {}
 	local function add_cmp(table, path)
+		print(pattern_table)
 		for _, pattern in pairs(pattern_table) do
+			print(pattern)
 			local handle = io.popen("rg -ttex '" .. pattern .. "' -IN --trim " .. path)
 			assert(handle)
 			local io_output = handle:read("*a")
