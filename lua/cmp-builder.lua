f792ca0114280f4001a54d87762f6930e72237ef
kinoshitalab <kouiti@arakkk.local>
2022-12-26 14:32:59 +0900

    update

--- lua/cmp-builder.lua
+++ lua/cmp-builder.lua
@@ -5,7 +5,7 @@ function M.add(pattern_table, cmp_name, trigger_characters, rg_option, replaceme
 	local cmp_table = {}
 	local function add_cmp(table, path)
 		for _, pattern in pairs(pattern_table) do
-			local handle = io.popen("rg -o " .. rg_option .. " '" .. pattern .. "' -IN --trim " .. path)
+			local handle = io.popen("rg -o " .. rg_option .. " '" .. pattern .. "' -IN --trim " .. string.gsub(path, " ", "\\ "))
 			if handle then
 				local io_output = handle:read("*a")
 				for line in io_output:gmatch("([^\n]*)\n?") do
