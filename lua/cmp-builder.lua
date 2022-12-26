69b4af544bd5b213939916270d238e9f4b8afe43
kinoshitalab <kouiti@arakkk.local>
2022-12-26 14:39:28 +0900

    update

--- lua/cmp-builder.lua
+++ lua/cmp-builder.lua
@@ -6,7 +6,7 @@ function M.add(pattern_table, cmp_name, trigger_characters, rg_option, replaceme
 	local function add_cmp(table, path)
 		for _, pattern in pairs(pattern_table) do
 			local handle = io.popen("rg -o " .. rg_option .. " '" .. pattern .. "' -IN --trim " .. path)
-			path = string.gsub(path, " ", "\\ ")
+			path = string.gsub(path, " ", "\\\\ ")
 			if handle ~= nil then
 				local io_output = handle:read("*a")
 				for line in io_output:gmatch("([^\n]*)\n?") do
